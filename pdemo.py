f0336f9f11b840bb26618c6e2912789d66923bd0
mental1234 <ramonesparza1234@gmail.com>
2019-02-20 14:23:13 -0600

    Finished python script

--- pdemo.py
+++ pdemo.py
@@ -1,8 +1,9 @@
 #! /usr/local/bin/python3
 
 import xml.etree.ElementTree as ET
-from github import Github
 from git import Repo
+from urllib.parse import urlparse
+
 
 ns = "http://maven.apache.org/POM/4.0.0"
 ET.register_namespace('', ns)
@@ -10,17 +11,17 @@ tree = ET.ElementTree()
 tree.parse('pom.xml')
 p = tree.getroot().find("{%s}version" %ns )
 
-print(p.text)
-
-g = Github("mental1234", "******")
-for repo in g.get_user().get_repos():
-    print(repo.name)
-    
-
-
 repo = Repo('.')
-repoName = repo.remote
-branch = repo.active_branch
 
+branch = repo.active_branch
 print(branch)
-print(repoName)
+url = repo.remotes.origin.url
+BRANCH=str(branch)
+
+org = urlparse(url)
+org=org.path.split('/')[1]
+print(org)
+final_string="ci_"+org+"_"+BRANCH+"-SNAPSHOT"
+print(final_string)
+p.text=final_string
+tree.write('pom.xml')
